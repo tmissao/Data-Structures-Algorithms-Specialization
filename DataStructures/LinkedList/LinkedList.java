172b3a9588d04554bf270268d18acf69e161ebd9
Tiago Missão <tiago_missao@bitbucket.org>
2018-02-24 11:14:02 -0300

    LinkedList

---
+++ DataStructures/LinkedList/LinkedList.java
@@ -0,0 +1,175 @@
+public class LinkedList<T> {
+
+  private Node<T> head;
+  private Node<T> tail;
+
+  public boolean isEmpty() {
+    return head == null;
+  }
+
+  public void addFirst(T key) {
+    Node<T> node = new Node<>(key);
+
+    if (head == null) {
+      head = node;
+      tail = node;
+      return;
+    }
+
+    node.setNext(head);
+    head = node;
+  }
+
+  public void addLast(T key) {
+    Node node = new Node<>(key);
+
+    if (tail == null) {
+      head = node;
+      tail = node;
+      return;
+    }
+
+    tail.setNext(node);
+    tail = node;
+  }
+
+  public T getFirst() {
+    return head != null ? head.getKey() : null;
+  }
+
+  public T getLast() {
+    return tail != null ? tail.getKey() : null;
+  }
+
+  public Node search(long key) {
+    Node node = head;
+
+    while(node != null) {
+      if (node.getKey().equals(key)) {
+        return node;
+      }
+      node = node.getNext();
+    }
+
+    return null;
+  }
+
+  private Node<T> searchPrevious(T key) {
+    Node<T> node = head;
+    Node<T> previous = null;
+
+    while(node != null) {
+
+      if (node.getKey().equals(key)) {
+        return previous;
+      }
+
+      previous = node;
+      node = node.getNext();
+    }
+
+    return null;
+  }
+
+  public T removeFirst() {
+    if (head == null) { return null; }
+
+    Node<T> node = head;
+
+    if (head == tail) {
+      tail = node.getNext();
+    }
+
+    head = node.getNext();
+
+    return node.getKey();
+  }
+
+  public T removeLast() {
+    if (tail == null) { return null; }
+
+    Node<T> node = tail;
+    Node<T> previous = null;
+
+    if (head == tail) {
+      head = node.getNext();
+      tail = node.getNext();
+    } else {
+      previous = searchPrevious(node.getKey());
+      if(previous != null) {
+        previous.setNext(node.getNext());
+      }
+      tail = previous;
+    }
+    return node.getKey();
+  }
+
+  public T remove(T key) {
+    Node<T> node = head;
+    Node<T> previous = null;
+
+    while(node != null) {
+      if (node.getKey().equals(key)) {
+        if (node == head) {
+          head = node.getNext();
+        }
+
+        if (node == tail) {
+          tail = previous;
+        }
+
+        if (previous != null) {
+          previous.setNext(node.getNext());
+        }
+
+        return node.getKey();
+      }
+      previous = node;
+      node = node.getNext();
+    }
+
+    return null;
+  }
+
+  public void print() {
+    Node node = head;
+    StringBuilder sb = new StringBuilder();
+
+    while(node != null) {
+      sb.append(node.getKey()).append(" ");
+      node = node.getNext();
+    }
+
+    System.out.println(sb);
+  }
+}
+
+class Node<T> {
+
+  private final T key;
+  private Node next;
+
+  public Node(T key) {
+    this.key = key;
+  }
+
+  public T getKey() {
+    return key;
+  }
+
+  public Node<T> getNext() {
+    return next;
+  }
+
+  public void setNext(Node next) {
+    this.next = next;
+  }
+
+  @Override
+  public String toString() {
+    final StringBuffer sb = new StringBuffer("Node{");
+    sb.append("key=").append(key);
+    sb.append('}');
+    return sb.toString();
+  }
+}
---
+++ DataStructures/LinkedList/LinkedListTest.java
@@ -0,0 +1,199 @@
+import static org.assertj.core.api.Assertions.*;
+import java.util.Random;
+
+public class LinkedListTest {
+
+  private LinkedList<Long> customList;
+  java.util.LinkedList<Long> list;
+
+  private Random random;
+
+  @org.junit.Before
+  public void setUp() throws Exception {
+    this.customList = new LinkedList<>();
+    list = new java.util.LinkedList<>();
+    this.random = new Random();
+  }
+
+  @org.junit.Test
+  public void isEmpty() throws Exception {
+    assertThat(customList.isEmpty()).isEqualTo(true);
+  }
+
+  @org.junit.Test
+  public void isNotEmpty() throws Exception {
+    Long value = random.nextLong();
+    customList.addFirst(value);
+    assertThat(customList.isEmpty()).isEqualTo(false);
+  }
+
+  @org.junit.Test
+  public void addFront() throws Exception {
+    for(int i = 0; i<50; i++) {
+      Long value = random.nextLong();
+      customList.addFirst(value);
+      assertThat(value).isEqualTo(customList.getFirst());
+    }
+  }
+
+  @org.junit.Test
+  public void addBack() throws Exception {
+    for(int i = 0; i<50; i++) {
+      Long value = random.nextLong();
+      customList.addLast(value);
+
+      assertThat(value).isEqualTo(customList.getLast());
+    }
+  }
+
+  @org.junit.Test
+  public void getFirst() throws Exception {
+    for(int i = 0; i<50; i++) {
+      Long value = random.nextLong();
+      customList.addFirst(value);
+      list.addFirst(value);
+
+      assertThat(list.getFirst()).isEqualTo(customList.getFirst());
+    }
+
+    for(int i = 0; i< 50; i++) {
+      assertThat(list.removeFirst()).isEqualTo(customList.removeFirst());
+    }
+  }
+
+  @org.junit.Test
+  public void getFirstEmpty() throws Exception {
+      assertThat(customList.getFirst()).isEqualTo(null);
+  }
+
+  @org.junit.Test
+  public void getLast() throws Exception {
+    for(int i = 0; i<50; i++) {
+      Long value = random.nextLong();
+      customList.addLast(value);
+      list.addLast(value);
+
+      assertThat(list.getLast()).isEqualTo(customList.getLast());
+    }
+
+    for(int i = 0; i< 50; i++) {
+      assertThat(list.removeFirst()).isEqualTo(customList.removeFirst());
+    }
+  }
+
+  @org.junit.Test
+  public void getLastEmpty() throws Exception {
+      assertThat(customList.getLast()).isEqualTo(null);
+  }
+
+  @org.junit.Test
+  public void searchEmpty() throws Exception {
+      assertThat(customList.search(random.nextLong())).isEqualTo(null);
+  }
+
+  @org.junit.Test
+  public void search() throws Exception {
+    for(int i = 0; i<50; i++) {
+      Long value = random.nextLong();
+      customList.addLast(value);
+
+      assertThat(customList.search(value).getKey()).isEqualTo(value);
+    }
+  }
+
+  @org.junit.Test
+  public void searchNotIn() throws Exception {
+    for(int i = 0; i<50; i++) {
+      Long value = (long) i;
+      customList.addLast(value);
+
+      assertThat(customList.search(50 + value)).isEqualTo(null);
+    }
+  }
+
+  @org.junit.Test
+  public void removeFirstEmpty() throws Exception {
+    assertThat(customList.removeFirst()).isEqualTo(null);
+  }
+
+  @org.junit.Test
+  public void removeFirst() throws Exception {
+    for(int i = 0; i<50; i++) {
+      Long value = random.nextLong();
+      customList.addLast(value);
+      list.addLast(value);
+
+      assertThat(list.getLast()).isEqualTo(customList.getLast());
+    }
+
+    for(int i = 0; i< 50; i++) {
+      assertThat(list.removeFirst()).isEqualTo(customList.removeFirst());
+    }
+  }
+
+  @org.junit.Test
+  public void removeLastEmpty() throws Exception {
+    assertThat(customList.removeLast()).isEqualTo(null);
+  }
+
+  @org.junit.Test
+  public void removeLast() throws Exception {
+    for(int i = 0; i<50; i++) {
+      Long value =  (long) i;//random.nextLong();
+      customList.addFirst(value);
+      list.addFirst(value);
+
+      assertThat(list.getLast()).isEqualTo(customList.getLast());
+    }
+
+    for(int i = 0; i< 50; i++) {
+      assertThat(list.removeLast()).isEqualTo(customList.removeLast());
+    }
+  }
+
+  @org.junit.Test
+  public void removeHeadTail() throws Exception {
+    customList.addFirst(10L);
+    assertThat(customList.remove(10L)).isEqualTo(10L);
+    assertThat(customList.getFirst()).isEqualTo(null);
+    assertThat(customList.getLast()).isEqualTo(null);
+  }
+
+  @org.junit.Test
+  public void removeHead() throws Exception {
+    customList.addFirst(10L);
+    customList.addLast(11L);
+    assertThat(customList.remove(10L)).isEqualTo(10L);
+    assertThat(customList.getFirst()).isEqualTo(11L);
+    assertThat(customList.getLast()).isEqualTo(11L);
+  }
+
+  @org.junit.Test
+  public void removeTail() throws Exception {
+    customList.addFirst(10L);
+    customList.addLast(11L);
+    assertThat(customList.remove(11L)).isEqualTo(11L);
+    assertThat(customList.getFirst()).isEqualTo(10L);
+    assertThat(customList.getLast()).isEqualTo(10L);
+
+  }
+
+  @org.junit.Test
+  public void removeMiddle() throws Exception {
+    customList.addFirst(10L);
+    customList.addLast(11L);
+    customList.addLast(12L);
+    assertThat(customList.remove(11L)).isEqualTo(11L);
+    assertThat(customList.getFirst()).isEqualTo(10L);
+    assertThat(customList.getLast()).isEqualTo(12L);
+
+    assertThat(customList.removeFirst()).isEqualTo(10L);
+    assertThat(customList.removeFirst()).isEqualTo(12L);
+    assertThat(customList.isEmpty()).isEqualTo(true);
+  }
+
+  @org.junit.Test
+  public void removeEmpty() throws Exception {
+    assertThat(customList.remove(10L)).isEqualTo(null);
+  }
+}
