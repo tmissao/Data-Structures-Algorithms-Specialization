487684ace04587d7167a488cf97955c10b04da33
Tiago Missão <t.missao@gmail.com>
2018-04-07 14:48:15 -0300

    Binary Tree

--- DataStructures/Structures/BinaryTree/BinaryTree.java
+++ DataStructures/Structures/BinaryTree/BinaryTree.java
@@ -3,201 +3,370 @@ import java.util.Queue;
 
 public class BinaryTree<T extends Comparable> {
 
-  /**
-   * Tree`s root element
-   */
-  private Node<T> root;
+    /**
+     * Tree`s root element
+     */
+    private Node<T> root;
 
-  private Node<T> insertLeaf(Node<T> node, T element) {
-    if (node == null) {
-      return new Node<>(element);
+    private void dstInOrder(Node<T> node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
+
+        dstInOrder(node.getLeft(), sb);
+        sb.append(" ").append(node.getKey());
+        dstInOrder(node.getRight(), sb);
     }
 
-    boolean isLess = node.getKey().compareTo(element) == 1;
+    private void dstPreOrder(Node<T> node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
 
-    if (isLess) {
-      node.setLeft(insertLeaf(node.getLeft(), element));
-    } else {
-      node.setRight(insertLeaf(node.getRight(), element));
+        sb.append(" ").append(node.getKey());
+        dstPreOrder(node.getLeft(), sb);
+        dstPreOrder(node.getRight(), sb);
     }
 
-    return node;
-  }
+    private void dstPostOrder(Node<T> node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
 
-  private void dstInOrder(Node<T> node, StringBuilder sb) {
-    if (node == null) {
-      return;
+        dstPostOrder(node.getLeft(), sb);
+        dstPostOrder(node.getRight(), sb);
+        sb.append(" ").append(node.getKey());
     }
 
-    dstInOrder(node.getLeft(), sb);
-    sb.append(" ").append(node.getKey());
-    dstInOrder(node.getRight(), sb);
-  }
-
-  private void dstPreOrder(Node<T> node, StringBuilder sb) {
-    if (node == null) {
-      return;
+    private long getHeight(Node<T> node, long height) {
+        if (node == null) {
+            return 0L;
+        }
+        return 1 + Math.max(getHeight(node.getLeft(), height), getHeight(node.getRight(), height));
     }
 
-    sb.append(" ").append(node.getKey());
-    dstPreOrder(node.getLeft(), sb);
-    dstPreOrder(node.getRight(), sb);
-  }
-
-  private void dstPostOrder(Node<T> node, StringBuilder sb) {
-    if (node == null) {
-      return;
+    private long getSize(Node<T> node) {
+        if (node == null) {
+            return 0L;
+        }
+        return 1 + getSize(node.getLeft()) + getSize(node.getRight());
     }
 
-    dstPostOrder(node.getLeft(), sb);
-    dstPostOrder(node.getRight(), sb);
-    sb.append(" ").append(node.getKey());
-  }
+    private Node<T> buildMirror(Node<T> node) {
+        if (node == null) {
+            return null;
+        }
 
-  private long getHeight(Node<T> node, long height) {
-    if (node == null) { return 0L; }
-    return 1 + Math.max(getHeight(node.getLeft(), height), getHeight(node.getRight(), height));
-  }
-
-  private long getSize(Node<T> node) {
-    if (node == null) { return 0L; }
-    return 1 + getSize(node.getLeft()) + getSize(node.getRight());
-  }
-
-  private Node<T> buildMirror(Node<T> node) {
-    if (node == null) {
-      return null;
+        Node<T> mirror = new Node<>(node.getKey());
+        mirror.setLeft(buildMirror(node.getRight()));
+        mirror.setRight(buildMirror(node.getLeft()));
+        return mirror;
     }
 
-    Node<T> mirror = new Node<>(node.getKey());
-    mirror.setLeft(buildMirror(node.getRight()));
-    mirror.setRight(buildMirror(node.getLeft()));
-    return mirror;
-  }
+    /**
+     * Searches by an element in the tree.
+     * If this element exists in the tree returns its node
+     * If this element not exists in the tree returns who should be its parent node
+     * If null is returned it means this element should be tree's root
+     * <p>
+     * Complexity: O(log(n)) if tree is balanced, otherwise O(n)
+     */
+    private Node<T> search(Node<T> node, T element) {
+        if (node == null) return null;
 
-  /**
-   * Inserts an element into tree
-   * Complexity: O(log(n)) if tree is balanced, otherwise O(n)
-   */
-  void insert(T element) {
-   root = insertLeaf(root, element);
-  }
+        if (node.getKey().equals(element)) {
+            return node;
+        }
 
-  /**
-   * Gets the string representation of the tree in order (Left, Node, Right)
-   * Complexity: O(n)
-   */
-  public String dstInOrder() {
-    StringBuilder sb = new StringBuilder();
-    dstInOrder(root, sb);
-    return sb.length() == 0 ? sb.toString() : sb.deleteCharAt(0).toString();
-  }
+        boolean isLess = node.getKey().compareTo(element) == 1;
 
-  /**
-   * Gets the string representation of the tree in pre order (Node, Left, Right)
-   * Complexity: O(n)
-   */
-  public String dstPreOrder() {
-    StringBuilder sb = new StringBuilder();
-    dstPreOrder(root, sb);
-    return sb.length() == 0 ? sb.toString() : sb.deleteCharAt(0).toString();
-  }
+        Node<T> result = search(isLess ? node.getLeft() : node.getRight(), element);
 
-  /**
-   * Gets the string representation of the tree in post order (Left, Right, Node)
-   * Complexity: O(n)
-   */
-  public String dstPostOrder() {
-    StringBuilder sb = new StringBuilder();
-    dstPostOrder(root, sb);
-    return sb.length() == 0 ? sb.toString() : sb.deleteCharAt(0).toString();
-  }
-
-  /**
-   * Gets the string representation by height
-   * Complexity: O(n)
-   */
-  public String bfs() {
-    StringBuilder sb = new StringBuilder();
-
-    if (root == null) {
-      return sb.toString();
+        // If the element is not in the tree returns who should be its parent
+        return result == null ? node : result;
     }
 
-    Queue<Node<T>> queue = new LinkedList<>();
-    queue.add(root);
+    /**
+     * Searches by the next Right Ancestor of a node
+     * Complexity: O(log(n)) if tree is balanced, otherwise O(n)
+     */
+    private Node<T> getRightAncestor(Node<T> node) {
+        if (node.getRight() == null) return null;
 
-    while(!queue.isEmpty()) {
-      Node<T> node = queue.remove();
+        Node ancestor = node.getRight();
 
-      if (node.getLeft() != null) {
-        queue.add(node.getLeft());
-      }
+        while (ancestor.getLeft() != null) {
+            ancestor = ancestor.getLeft();
+        }
 
-      if (node.getRight() != null) {
-        queue.add(node.getRight());
-      }
-
-      sb.append(" ").append(node.getKey());
+        return ancestor;
     }
 
-    return sb.deleteCharAt(0).toString();
-  }
+    /**
+     * Searches by the next Left Descendent of a node
+     * Complexity: O(log(n)) if tree is balanced, otherwise O(n)
+     */
+    private Node<T> getLeftDescendant(Node<T> node) {
+        if (node == null) return null;
 
-  /**
-   * Get the tree`s height
-   * Complexity: (n)
-   */
-  public Long getHeight() {
-    return getHeight(root, 0);
-  }
+        Node descendant = node.getParent();
 
-  /**
-   * Get the number of elements in the tree
-   * Complexity: (n)
-   */
-  public Long getSize() {
-    return getSize(root);
-  }
+        while (descendant != null && node.getKey().compareTo(descendant.getKey()) == 1) {
+            descendant = descendant.getParent();
+        }
 
-  /**
-   * Builds a mirror representation of the tree
-   * Complexity: (n)
-   */
-  public BinaryTree<T> getMirror() {
-    BinaryTree<T> mirror = new BinaryTree<>();
-    mirror.root = mirror.buildMirror(root);
-    return mirror;
-  }
+        return descendant;
+    }
+
+    /**
+     * Searches by the next element of a currently element
+     * If there is not a next element null is returned
+     * <p>
+     * Complexity: O(log(n)) if tree is balanced, otherwise O(n)
+     */
+    private Node<T> next(Node<T> node) {
+        if (node == null) return null;
+
+        // If the node has a right child the next element is the first node on the right
+        // that does not have a left child
+        if (node.getRight() != null) {
+            return getRightAncestor(node);
+        }
+
+        // If the node does not have a right child the next element descendant (parent) that is
+        // greater than its value
+        return getLeftDescendant(node);
+    }
+
+    /**
+     * Inserts an element into tree
+     * Complexity: O(log(n)) if tree is balanced, otherwise O(n)
+     */
+    public void insert(T element) {
+        Node<T> node = new Node<>(element);
+        Node<T> parent = search(root, element);
+
+        if (parent == null) {
+            root = node;
+            return;
+        }
+
+        node.setParent(parent);
+
+        boolean isLess = parent.getKey().compareTo(element) == 1;
+
+        if (isLess) {
+            parent.setLeft(node);
+        } else {
+            parent.setRight(node);
+        }
+    }
+
+    /**
+     * Deletes an element in tree
+     * Complexity: O(log(n)) if tree is balanced, otherwise O(n)
+     */
+    public void delete(T element) {
+        Node<T> result = search(root, element);
+
+        if (result == null || result.getKey().compareTo(element) != 0) {
+            return;
+        }
+
+
+        if (result.getRight() == null) {
+            // indicates if the deleted node is left or right child of its parent
+            boolean isLeft = result.getParent().getLeft().equals(result);
+
+            if (isLeft) {
+                result.getParent().setLeft(result.getLeft());
+            } else {
+                result.getParent().setRight(result.getLeft());
+            }
+
+            if (result.getLeft() != null) {
+                result.getLeft().setParent(result.getParent());
+            }
+        } else {
+            Node<T> next = next(result);
+            result.setKey(next.getKey());
+
+            // indicates if the deleted node is left or right child of its parent
+            boolean isLeft = next.getParent().getLeft().equals(next);
+            if (isLeft) {
+                next.getParent().setLeft(next.getRight());
+            } else {
+                next.getParent().setRight(next.getRight());
+            }
+
+            if (next.getRight() != null) {
+                next.getRight().setParent(next.getParent());
+            }
+        }
+
+    }
+
+    /**
+     * Gets the string representation of the tree in order (Left, Node, Right)
+     * Complexity: O(n)
+     */
+    public String dstInOrder() {
+        StringBuilder sb = new StringBuilder();
+        dstInOrder(root, sb);
+        return sb.length() == 0 ? sb.toString() : sb.deleteCharAt(0).toString();
+    }
+
+    /**
+     * Gets the string representation of the tree in pre order (Node, Left, Right)
+     * Complexity: O(n)
+     */
+    public String dstPreOrder() {
+        StringBuilder sb = new StringBuilder();
+        dstPreOrder(root, sb);
+        return sb.length() == 0 ? sb.toString() : sb.deleteCharAt(0).toString();
+    }
+
+    /**
+     * Gets the string representation of the tree in post order (Left, Right, Node)
+     * Complexity: O(n)
+     */
+    public String dstPostOrder() {
+        StringBuilder sb = new StringBuilder();
+        dstPostOrder(root, sb);
+        return sb.length() == 0 ? sb.toString() : sb.deleteCharAt(0).toString();
+    }
+
+    /**
+     * Gets the string representation by height
+     * Complexity: O(n)
+     */
+    public String bfs() {
+        StringBuilder sb = new StringBuilder();
+
+        if (root == null) {
+            return sb.toString();
+        }
+
+        Queue<Node<T>> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node<T> node = queue.remove();
+
+            if (node.getLeft() != null) {
+                queue.add(node.getLeft());
+            }
+
+            if (node.getRight() != null) {
+                queue.add(node.getRight());
+            }
+
+            sb.append(" ").append(node.getKey());
+        }
+
+        return sb.deleteCharAt(0).toString();
+    }
+
+    /**
+     * Search by a specific element in the tree
+     * <p>
+     * Complexity: O(log(n)) if tree is balanced, otherwise O(n)
+     */
+    public T search(T element) {
+        Node<T> result = search(root, element);
+
+        // Since search method returns null it tree is empty or who should be parent of an element
+        // in case that is does not exists in the tree, it is necessary to check if the result is the
+        // element that is searching by.
+        return result == null || result.getKey().compareTo(element) != 0 ? null : result.getKey();
+    }
+
+    public String rangeSearch(T start, T end) {
+        StringBuffer sb = new StringBuffer("");
+
+        if (start.compareTo(end) == 1) {
+            return sb.toString();
+        }
+
+        Node<T> node = search(root, start);
+
+        while (node != null && node.getKey().compareTo(end) != 1) {
+            if (start.compareTo(node.getKey()) != 1) {
+                sb.append(" ").append(node.getKey());
+            }
+            node = next(node);
+        }
+
+        return sb.length() != 0 ? sb.deleteCharAt(0).toString() : sb.toString();
+    }
+
+    /**
+     * Get the tree`s height
+     * Complexity: (n)
+     */
+    public Long getHeight() {
+        return getHeight(root, 0);
+    }
+
+    /**
+     * Get the number of elements in the tree
+     * Complexity: (n)
+     */
+    public Long getSize() {
+        return getSize(root);
+    }
+
+    /**
+     * Builds a mirror representation of the tree
+     * Complexity: (n)
+     */
+    public BinaryTree<T> getMirror() {
+        BinaryTree<T> mirror = new BinaryTree<T>();
+        mirror.root = mirror.buildMirror(root);
+        return mirror;
+    }
 }
 
-class Node<T extends Comparable>  {
+class Node<T extends Comparable> {
 
-  public Node(T key) {
-    this.key = key;
-  }
+    private T key;
+    private Node<T> parent;
+    private Node<T> right;
+    private Node<T> left;
 
-  private final T key;
-  private Node<T> right;
-  private Node<T> left;
+    public Node(T key) {
+        this.key = key;
+    }
 
-  public T getKey() {
-    return key;
-  }
+    public T getKey() {
+        return key;
+    }
 
-  public Node<T> getRight() {
-    return right;
-  }
+    public void setKey(T key) {
+        this.key = key;
+    }
 
-  public void setRight(Node<T> right) {
-    this.right = right;
-  }
+    public Node<T> getRight() {
+        return right;
+    }
 
-  public Node<T> getLeft() {
-    return left;
-  }
+    public void setRight(Node<T> right) {
+        this.right = right;
+    }
 
-  public void setLeft(Node<T> left) {
-    this.left = left;
-  }
+    public Node<T> getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public Node<T> getParent() {
+        return parent;
+    }
+
+    public void setParent(Node<T> parent) {
+        this.parent = parent;
+    }
 }
--- DataStructures/Structures/BinaryTree/BinaryTreeTest.java
+++ DataStructures/Structures/BinaryTree/BinaryTreeTest.java
@@ -4,87 +4,139 @@ import static org.assertj.core.api.Assertions.*;
 
 public class BinaryTreeTest {
 
-  private BinaryTree<Long> tree;
+    private BinaryTree<Long> tree;
+    private long[] inputs = {10,5,2,0,3,4,6,8,7,9,13,20,17,15,11, 16};
 
-  private void insertTest() {
-    long[] inputs = {10,5,2,0,3,4,6,8,7,9,13,20,17,15,11,20,20,20};
-    for(long n : inputs) tree.insert(n);
-  }
+    private void insertTest() {
+        for(long n : inputs) tree.insert(n);
+    }
 
-  @Before
-  public void setUp() throws Exception {
-    this.tree = new BinaryTree<>();
-    insertTest();
-  }
+    @Before
+    public void setUp() throws Exception {
+        this.tree = new BinaryTree<>();
+        insertTest();
+    }
 
 
-  @Test
-  public void dstInOrder() throws Exception {
-    assertThat(tree.dstInOrder()).isEqualTo("0 2 3 4 5 6 7 8 9 10 11 13 15 17 20 20 20 20");
-  }
+    @Test
+    public void dstInOrder() throws Exception {
+        assertThat(tree.dstInOrder()).isEqualTo("0 2 3 4 5 6 7 8 9 10 11 13 15 16 17 20");
+    }
 
-  @Test
-  public void dstInOrderEmpty() throws Exception {
-    assertThat(new BinaryTree<Long>().dstInOrder()).isEqualTo("");
-  }
+    @Test
+    public void dstInOrderEmpty() throws Exception {
+        assertThat(new BinaryTree<Long>().dstInOrder()).isEqualTo("");
+    }
 
-  @Test
-  public void dstPreOrder() throws Exception {
-    assertThat(tree.dstPreOrder()).isEqualTo("10 5 2 0 3 4 6 8 7 9 13 11 20 17 15 20 20 20");
-  }
+    @Test
+    public void dstPreOrder() throws Exception {
+        assertThat(tree.dstPreOrder()).isEqualTo("10 5 2 0 3 4 6 8 7 9 13 11 20 17 15 16");
+    }
 
-  @Test
-  public void dstPreOrderEmpty() throws Exception {
-    assertThat(new BinaryTree<Long>().dstPreOrder()).isEqualTo("");
-  }
+    @Test
+    public void dstPreOrderEmpty() throws Exception {
+        assertThat(new BinaryTree<Long>().dstPreOrder()).isEqualTo("");
+    }
 
-  @Test
-  public void dstPostOrder() throws Exception {
-    assertThat(tree.dstPostOrder()).isEqualTo("0 4 3 2 7 9 8 6 5 11 15 17 20 20 20 20 13 10");
-  }
+    @Test
+    public void dstPostOrder() throws Exception {
+        assertThat(tree.dstPostOrder()).isEqualTo("0 4 3 2 7 9 8 6 5 11 16 15 17 20 13 10");
+    }
 
-  @Test
-  public void dstPostOrderEmpty() throws Exception {
-    assertThat(new BinaryTree<Long>().dstPostOrder()).isEqualTo("");
-  }
+    @Test
+    public void dstPostOrderEmpty() throws Exception {
+        assertThat(new BinaryTree<Long>().dstPostOrder()).isEqualTo("");
+    }
 
-  @Test
-  public void bfs() throws Exception {
-    assertThat(tree.bfs()).isEqualTo("10 5 13 2 6 11 20 0 3 8 17 20 4 7 9 15 20 20");
-  }
+    @Test
+    public void bfs() throws Exception {
+        assertThat(tree.bfs()).isEqualTo("10 5 13 2 6 11 20 0 3 8 17 4 7 9 15 16");
+    }
 
-  @Test
-  public void bfsEmpty() throws Exception {
-    assertThat(new BinaryTree<Long>().bfs()).isEqualTo("");
-  }
+    @Test
+    public void bfsEmpty() throws Exception {
+        assertThat(new BinaryTree<Long>().bfs()).isEqualTo("");
+    }
 
-  @Test
-  public void getHeight() throws Exception {
-    assertThat(tree.getHeight()).isEqualTo(6);
-  }
+    @Test
+    public void getHeight() throws Exception {
+        assertThat(tree.getHeight()).isEqualTo(6);
+    }
 
-  @Test
-  public void getHeightEmpty() throws Exception {
-    assertThat(new BinaryTree<Long>().getHeight()).isEqualTo(0);
-  }
+    @Test
+    public void getHeightEmpty() throws Exception {
+        assertThat(new BinaryTree<Long>().getHeight()).isEqualTo(0);
+    }
 
-  @Test
-  public void getSize() throws Exception {
-    assertThat(tree.getSize()).isEqualTo(18);
-  }
+    @Test
+    public void getSize() throws Exception {
+        assertThat(tree.getSize()).isEqualTo(inputs.length);
+    }
 
-  @Test
-  public void getSizeEmpty() throws Exception {
-    assertThat(new BinaryTree<Long>().getSize()).isEqualTo(0);
-  }
+    @Test
+    public void getSizeEmpty() throws Exception {
+        assertThat(new BinaryTree<Long>().getSize()).isEqualTo(0);
+    }
 
-  @Test
-  public void getMirror() throws Exception {
-    assertThat(tree.getMirror().dstInOrder()).isEqualTo("20 20 20 20 17 15 13 11 10 9 8 7 6 5 4 3 2 0");
-  }
+    @Test
+    public void getMirror() throws Exception {
+        assertThat(tree.getMirror().dstInOrder()).isEqualTo("20 17 16 15 13 11 10 9 8 7 6 5 4 3 2 0");
+    }
 
-  @Test
-  public void getMirrorEmpty() throws Exception {
-    assertThat(new BinaryTree<Long>().getMirror().dstInOrder()).isEqualTo("");
-  }
+    @Test
+    public void search() throws Exception {
+        assertThat(tree.search(20L)).isEqualTo(20L);
+        assertThat(tree.search(11L)).isEqualTo(11L);
+        assertThat(tree.search(4l)).isEqualTo(4L);
+        assertThat(tree.search(51L)).isNull();
+        assertThat(tree.search(18L)).isNull();
+        assertThat(tree.search(16L)).isEqualTo(16L);
+        assertThat(tree.search(0L)).isEqualTo(0L);
+        assertThat(tree.search(-1L)).isNull();
+    }
+
+    @Test
+    public void rangeSearch() throws Exception {
+        assertThat(tree.rangeSearch(20L, 20L)).isEqualTo("20");
+        assertThat(tree.rangeSearch(4L, 15L)).isEqualTo("4 5 6 7 8 9 10 11 13 15");
+        assertThat(tree.rangeSearch(-4L, -1L)).isEqualTo("");
+        assertThat(tree.rangeSearch(-4L, 1L)).isEqualTo("0");
+        assertThat(tree.rangeSearch(4L, 1L)).isEqualTo("");
+        assertThat(tree.rangeSearch(1L, 4L)).isEqualTo("2 3 4");
+    }
+
+    @Test
+    public void deleteNoRightChild() throws Exception {
+        assertThat(tree.getSize()).isEqualTo(inputs.length);
+        assertThat(tree.bfs()).isEqualTo("10 5 13 2 6 11 20 0 3 8 17 4 7 9 15 16");
+        assertThat(tree.rangeSearch(12L, 20L)).isEqualTo("13 15 16 17 20");
+        assertThat(tree.getHeight()).isEqualTo(6);
+
+        tree.delete(20L);
+
+        assertThat(tree.getSize()).isEqualTo(inputs.length - 1);
+        assertThat(tree.bfs()).isEqualTo("10 5 13 2 6 11 17 0 3 8 15 4 7 9 16");
+        assertThat(tree.rangeSearch(12L, 20L)).isEqualTo("13 15 16 17");
+        assertThat(tree.getHeight()).isEqualTo(5);
+    }
+
+    @Test
+    public void deleteWithRightChild() throws Exception {
+        assertThat(tree.getSize()).isEqualTo(inputs.length);
+        assertThat(tree.bfs()).isEqualTo("10 5 13 2 6 11 20 0 3 8 17 4 7 9 15 16");
+        assertThat(tree.rangeSearch(12L, 20L)).isEqualTo("13 15 16 17 20");
+        assertThat(tree.getHeight()).isEqualTo(6);
+
+        tree.delete(13L);
+
+        assertThat(tree.getSize()).isEqualTo(inputs.length - 1);
+        assertThat(tree.bfs()).isEqualTo("10 5 15 2 6 11 20 0 3 8 17 4 7 9 16");
+        assertThat(tree.rangeSearch(12L, 20L)).isEqualTo("15 16 17 20");
+        assertThat(tree.getHeight()).isEqualTo(5);
+    }
+
+    @Test
+    public void getMirrorEmpty() throws Exception {
+        assertThat(new BinaryTree<Long>().getMirror().dstInOrder()).isEqualTo("");
+    }
 }
